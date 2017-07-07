736ac212ec5bd634dc1d83383dadef46c861c8aa
Matkovic123 <toni.matkovic0@gmail.com>
2017-07-07 10:24:17 +0200

    Initial commit

---
+++ src/main/java/hr/tvz/matkovic/PeriodniSustavElemenataApplication.java
@@ -0,0 +1,12 @@
+package hr.tvz.matkovic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodniSustavElemenataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodniSustavElemenataApplication.class, args);
+	}
+}
