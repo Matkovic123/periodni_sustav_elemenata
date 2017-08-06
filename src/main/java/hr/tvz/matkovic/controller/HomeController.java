d166d367caccbc4a76d78a7a690bf497d440f0b5
Matkovic123 <toni.matkovic0@gmail.com>
2017-08-06 14:11:28 +0200

    xc asjejsjr

--- src/main/java/hr/tvz/matkovic/controller/HomeController.java
+++ src/main/java/hr/tvz/matkovic/controller/HomeController.java
@@ -2,6 +2,7 @@ package hr.tvz.matkovic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Matkovic on 07/07/2017.
@@ -12,7 +13,7 @@ public class HomeController {
     // --- VIEW NAMES ---------------------------------------------------------
     public static final String PERIODIC_SYSTEM_REDIRECT = "redirect:/periodic_table";
 
-    @GetMapping({"/", "/index"})
+    @RequestMapping("/")
     public String home(){
         return PERIODIC_SYSTEM_REDIRECT;
     }
